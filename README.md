1debaa7d5f2066ce751c6b2a4e3dface3e0e063c
Alex Poon <alexpoontech@gmail.com>
2017-07-28 15:58:31 +0800

    　

---
+++ README.md
@@ -0,0 +1,5 @@
+# Civ VI Localisation Fix
+## Windows
+Install the `patch` utility from [GnuWin32](http://gnuwin32.sourceforge.net) first.
+## Unix-like
+Execute `apply.sh` in the sub-folder.
---
+++ zh_Hant_HK/apply.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Copyleft 2017.
+#
+patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/base/assets/text" < zh_Hant_HK.patch
+# Reserved for DLCs and mods.
