e4bfc06f6aa00916cb9db3ab2787c82e894e8a82
Alex Poon <alexpoontech@gmail.com>
2017-07-30 01:32:58 +0800

    　

--- zh_Hant_HK/apply.sh
+++ zh_Hant_HK/apply.sh
@@ -2,5 +2,17 @@
 #
 # Copyleft 2017.
 #
-patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/base/assets/text" < zh_Hant_HK.patch
-# Reserved for DLCs and mods.
+patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/base/assets/text/vanilla_zh_hant_hk.xml" < vanilla_zh_hant_hk.patch
+if [ -d "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/australia/text" ]; then
+  patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/\
+australia/text/australia_translations_text.xml" < australia_translations_text.patch
+  patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/\
+australia/text/australia_translations_configtext.xml" < australia_translations_configtext.patch
+fi
+if [ -d "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/poland_jadwiga/text" ]; then
+  patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/\
+poland_jadwiga/text/poland_translations_text.xml" < poland_translations_text.patch
+  patch "${HOME}/.steam/steam/steamapps/common/Sid Meier's Civilization VI/steamassets/dlc/\
+poland_jadwiga/text/poland_translations_configtext.xml" < poland_translations_configtext.patch
+fi
+# Reserved for mods.
